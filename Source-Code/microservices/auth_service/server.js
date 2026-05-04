c25f854cff686e4b3e0353a9afd3c8a2713e951c
22chiragA <122338784+22chiragA@users.noreply.github.com>
2026-05-04 22:17:50 +0530

    Create server.js

---
+++ Source-Code/microservices/auth_service/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Auth + Transaction + Account in ONE app
+
+app.post('/login', (req, res) => {
+    res.json({ message: "Login Success (Monolithic)" });
+});
+
+app.post('/transfer', (req, res) => {
+    const { amount } = req.body;
+    res.json({ message: ⁠ Transferred ₹${amount} (Monolithic) ⁠ });
+});
+
+app.get('/balance', (req, res) => {
+    res.json({ balance: 5000 });
+});
+
+app.listen(3000, () => {
+    console.log("Monolithic App running on port 3000");
+});
