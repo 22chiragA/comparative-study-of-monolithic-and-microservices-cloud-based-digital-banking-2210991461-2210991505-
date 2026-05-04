dc00d68b181a4fcdd5e1baf0e1d7dd3c8756bbae
22chiragA <122338784+22chiragA@users.noreply.github.com>
2026-05-04 22:20:56 +0530

    Update server.js

--- Source-Code/microservices/auth_service/server.js
+++ Source-Code/microservices/auth_service/server.js
@@ -3,21 +3,10 @@ const app = express();
 
 app.use(express.json());
 
-// Auth + Transaction + Account in ONE app
-
 app.post('/login', (req, res) => {
-    res.json({ message: "Login Success (Monolithic)" });
+    res.json({ message: "Login Success (Auth Service)" });
 });
 
-app.post('/transfer', (req, res) => {
-    const { amount } = req.body;
-    res.json({ message: ⁠ Transferred ₹${amount} (Monolithic) ⁠ });
-});
-
-app.get('/balance', (req, res) => {
-    res.json({ balance: 5000 });
-});
-
-app.listen(3000, () => {
-    console.log("Monolithic App running on port 3000");
+app.listen(3001, () => {
+    console.log("Auth Service running on port 3001");
 });
